7f684283a022aa36ce4f931d94f79600d117cdec
Khushi Arora <khushiarora752002@gmail.com>
2022-09-03 23:51:35 +0530

    adding compo

---
+++ src/Components/Experience/Exp.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '.Exp.css';
+
+const Exp = () => {
+  return (
+    <div>Exp</div>
+  )
+}
+
+export default Exp;
---
+++ src/Components/Testimonials/Testi.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Testi.css';
+
+const Testi = () => {
+  return (
+    <div>Testi</div>
+  )
+}
+
+export default Testi;
