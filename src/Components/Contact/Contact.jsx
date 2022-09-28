81128a99a7d346e406e27128cc70fa5668a060d5
Khushi Arora <khushiarora752002@gmail.com>
2022-09-29 00:10:41 +0530

    changes

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Contact.css';
+import {MdOutlineEmail} from 'react-icons/md';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_xsccto7', 'template_brp7of7', form.current, 'ocK3BDqaZ_4dpbv11')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
+
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container con__container">
+        <div className="con_options">
+          <article className="con__option">
+            <MdOutlineEmail className="con__option-icon"/>
+            <h4>Email</h4>
+            <h5>khushiarora752002@gmail.com</h5>
+            <a href="mailto:khushiarora752002@gmail.com">Send an Email</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text"  name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="msg" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/Components/Portfolio/Port.jsx
+++ src/Components/Portfolio/Port.jsx
@@ -5,6 +5,38 @@ import IMG2 from '../../Assets/2.jpg';
 import IMG3 from '../../Assets/3.jpg';
 import IMG4 from '../../Assets/4.jpg';
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'FLIGHT BOOKING SYSTEM YATRI.COM',
+    github: 'https://github.com/KhushiArora5/Doosra-College/tree/main/Flight-Booking',
+    demo: 'https://chimerical-pasca-8e29be.netlify.app/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'COFFEE ROOSTERS CLONE',
+    github: 'https://github.com/KhushiArora5/CoffeeRoaster',
+    demo: 'https://resilient-cranachan-9e61cc.netlify.app/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'TIC TAC TOE',
+    github: 'https://github.com/KhushiArora5/Tic-Tac-Toe',
+    demo: 'https://steady-kashata-47d6ca.netlify.app/'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'E-COMMERCE SWIFT',
+    github: 'https://github.com/KhushiArora5/e-commerce',
+    demo: 'https://superb-capybara-6f4ee8.netlify.app/'
+  }
+
+]
+
 const Port = () => {
   return (
     <section id="port">
@@ -12,7 +44,7 @@ const Port = () => {
       <h2>Portfolio</h2>
 
       <div className="container port__container">
-      <article className="port__item">
+        {/* <article className="port__item">
           <div className="port__item-image">
             <img src={IMG1} alt="1" />
           </div>
@@ -54,7 +86,23 @@ const Port = () => {
             <a href="https://github.com/KhushiArora5/e-commerce" className="btn">GitHub</a>
             <a href="https://superb-capybara-6f4ee8.netlify.app/" className="btn btn-primary">Live Demo</a>
           </div>
-        </article>
+        </article> */}
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className="port__item">
+                <div className="port__item-image">
+                  <img src={image} alt="1" />
+                </div>
+                <h3>{title}</h3>
+                <div className="port__item-cta">
+                  <a href={github} className="btn">GitHub</a>
+                  <a href={demo} className="btn btn-primary">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
